3bda066319fc981dffb0872ad06c68573e18f0d8
Benhur Tekeste <113864743+benichi2022@users.noreply.github.com>
2024-04-07 13:45:29 +0000

    Added html and css

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-  return "<p>Hello World</p>"
+  return render_template('home.html')
 
+
+#favicon using flask
+# @app.route('/favicon.ico')
+# def favicon():
+#   return app.send_static_file('favicon.ico')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
